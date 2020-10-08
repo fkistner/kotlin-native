fe4738474f3a99e18b62637fdce5b49d8593400b
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2020-10-08 10:56:22 +0300

    fixup! Prevent Obj-C dealloc accessing Kotlin counterpart

--- backend.native/tests/interop/objc/tests/kt41811.m
+++ backend.native/tests/interop/objc/tests/kt41811.m
@@ -2,11 +2,36 @@
 #import "assert.h"
 
 id retainObject = nil;
+BOOL deallocRetainReleaseDeallocated = NO;
 
 @implementation DeallocRetainRelease
 -(void)dealloc {
     retainObject = self;
     assert(retainObject == self);
     retainObject = nil;
+
+    assert(!deallocRetainReleaseDeallocated);
+    deallocRetainReleaseDeallocated = YES;
+}
+@end;
+
+@implementation ObjCWeakReference
+@end;
+
+id <WeakReference> weakDeallocLoadWeak = nil;
+BOOL deallocLoadWeakDeallocated = NO;
+
+@implementation DeallocLoadWeak
+-(void)checkWeak {
+    assert(weakDeallocLoadWeak != nil);
+    assert(weakDeallocLoadWeak.referent == self);
+}
+
+-(void)dealloc {
+    assert(weakDeallocLoadWeak != nil);
+    assert(weakDeallocLoadWeak.referent == nil);
+
+    assert(!deallocLoadWeakDeallocated);
+    deallocLoadWeakDeallocated = YES;
 }
 @end;
